fe73b8ccafad0a0c4ec7384be10c56ffdd57e2da
Michael Kampouris <86621340+MikeusAurelius@users.noreply.github.com>
2022-04-19 12:29:55 -0400

    Created FeedItem

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Michael Kampouris on 4/19/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: String
+}

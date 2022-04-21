240432c5c38b17bf4c22d0881bd26a60af18a867
Michael Kampouris <86621340+MikeusAurelius@users.noreply.github.com>
2022-04-21 12:28:25 -0400

    Unit Tests - Remote Feed Loader
    RemoteFeedLoader does not request data upon creation.

---
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,29 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Michael Kampouris on 4/21/22.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    
+    var requestedURL: URL?
+    
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+                
+        XCTAssertNil(client.requestedURL)
+    }
+
+}
